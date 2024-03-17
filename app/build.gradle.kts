59f85faa9c67dc4580581d12faf833a9d50a263e
jesus puentes <agentej1718@gmail.com>
2024-03-17 14:24:01 -0500

    diseno del from1 terminado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "pe.edu.idat.evc3"
-        minSdk = 24
+        minSdk = 33
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
